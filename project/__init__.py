24255d98f54afe3fb5941cab8e9740af238c955c
MIRARG20 <amiraragab480@gmail.com>
2025-01-05 13:18:50 +0200

    Set up SQLalchemy and configure database URI

--- project/__init__.py
+++ project/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 
 
@@ -6,5 +8,9 @@ from flask import Flask
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] ='8a67af50401ddca8fc961009b0c0f3f2e49bd3ef91a4b0cdb150fc83dec7469c'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 
+db = SQLAlchemy(app)
