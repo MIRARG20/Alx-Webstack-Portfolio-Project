58e9f4b76fe3ef6214cdcf0a2e29695ae452a08d
MIRARG20 <amiraragab480@gmail.com>
2025-01-05 15:03:18 +0200

    create a RegistrationForm and implement username validation

---
+++ project/forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from project.models import Customer
+
+
+# Form for user registration.
+class RegistrationForm(FlaskForm):
+    username = StringField(label='Username', validators=[Length(min=2, max=30), DataRequired()])
+    email = StringField(label='Email', validators=[Email(), DataRequired()])
+    password = PasswordField(label='Password', validators=[Length(min=6), DataRequired()])
+    confirm_password = PasswordField(label='Confirm_password', validators=[EqualTo('password'), DataRequired()])
+    submit = SubmitField(label='Sign Up')
+    
+    def validate_username(self, username):
+        customer = Customer.query.filter_by(username=username.data).first()
+        if customer:
+            raise ValidationError('Username already exists')
