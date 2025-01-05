84b4a03e91f1877b42e1759d11d111327268d5cb
MIRARG20 <amiraragab480@gmail.com>
2025-01-05 16:51:10 +0200

    create a LoginForm class

--- project/forms.py
+++ project/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from project.models import Customer
 
@@ -16,3 +16,12 @@ class RegistrationForm(FlaskForm):
         customer = Customer.query.filter_by(username=username.data).first()
         if customer:
             raise ValidationError('Username already exists')
+        
+
+
+# Form for user login.
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[Email(), DataRequired()])
+    password = PasswordField(label='Password', validators=[Length(min=6), DataRequired()])
+    remember = BooleanField(label='Remember Me')
+    submit = SubmitField(label='Sign in')
